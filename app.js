60a53086ecd29c9c4c0cc1f81b3387c8616bcb56
Miguel <mmirandasanchez16@gmail.com>
2024-03-05 11:10:52 -0300

    copia de seguridad

--- app.js
+++ app.js
@@ -7,3 +7,33 @@ let cargarApp = () => {
   cargarIngresos();
   cargarEgresos();
 };
+
+let totalIngresos = () => {
+  let totalIngresos = 0;
+  for (let ingreso of ingresos) {
+    totalIngresos += ingreso.valor;
+  }
+  return totalIngresos;
+};
+
+let totalEgresos = () => {
+  let totalEgresos = 0;
+  for (let egreso of egresos) {
+    totalEgresos += egreso.valor;
+  }
+  return totalEgresos;
+};
+
+let cargarCabecero = () => {
+  let presupuesto = totalIngresos() - totalEgresos();
+
+  let procentaje = totalEgresos() / totalIngresos();
+
+  document.getElementById("presupuesto").innerHTML = formatoMoneda(presupuesto);
+  document.getElementById("porcentaje").innerHTML =
+    foramatoPorcentaje(procentaje);
+  document.getElementById("ingresos").innerHTML = formatoMoneda(
+    totalIngresos()
+  );
+  document.getElementById("egresos").innerHTML = formatoMoneda(totalEgresos());
+};
