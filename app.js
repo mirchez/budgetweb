3582af53b33188056b718d83c5583770ff5d7145
Miguel <mmirandasanchez16@gmail.com>
2024-03-08 21:35:09 -0300

    copia de seguridad

--- app.js
+++ app.js
@@ -115,3 +115,28 @@ const crearEgresoHTML = (egresos) => {
     `;
   return egresoHTML;
 };
+
+const eliminarEgreso = (id) => {
+  let indiceEliminar = egresos.findIndex((egresos) => egresos.id === id);
+  egresos.splice(indiceEliminar, 1);
+  cargarCabecero();
+  cargarEgresos();
+};
+
+let agregarDato = () => {
+  let forma = document.forms["forma"];
+  let tipo = forma["tipo"];
+  let descripcion = forma["descripcion"];
+  let valor = forma["valor"];
+  if (descripcion.value !== "" && valor.value !== "") {
+    if (tipo.value === "ingreso") {
+      ingresos.push(new Ingreso(descripcion.value, +valor.value));
+      cargarCabecero();
+      cargarIngresos();
+    } else if (tipo.value === "egreso") {
+      egresos.push(new Egreso(descripcion.value, +valor.value));
+      cargarCabecero();
+      cargarEgresos();
+    }
+  }
+};
